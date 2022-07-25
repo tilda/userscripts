c569d98cef92bb0445565fceefbcf2a738b7510b
tilda <iamtheworst@programmer.net>
2022-07-24 19:06:54 -0500

    headcount 1.1: fix compat on firefox
    The previous implementation didn't work on Firefox due to the script running too early. This commit fixes it by adding a MutationObserver to the page root so it runs when the friends list is populated.

--- headcount/headcount.user.js
+++ headcount/headcount.user.js
@@ -1,7 +1,7 @@
-/// ==UserScript==
+// ==UserScript==
 // @name         Headcount
 // @namespace    https://osu.ppy.sh/
-// @version      1.0
+// @version      1.1
 // @updateURL    https://raw.githubusercontent.com/tilda/userscripts/main/headcount/headcount.user.js
 // @downloadURL  https://raw.githubusercontent.com/tilda/userscripts/main/headcount/headcount.user.js
 // @description  Count & percentage of mutual friends on osu-web's friends page
@@ -12,29 +12,36 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    window.onload = (event) => {
-        const percentage = function(partial, total) {
-            return ((100 * partial) / total).toFixed(2)
-        }
-        let body = document.body
-        let addedFriends = body.getElementsByClassName("user-card-brick").length
-        let mutualFriends = body.getElementsByClassName("user-card-brick--mutual").length
-        const toolbarRow = body.getElementsByClassName("user-list__toolbar-row")[0]
-        const toolbarItem = document.createElement('div')
-        const spacing = document.createElement('div')
-        const toolbarItemContent = document.createTextNode(`${mutualFriends}/${addedFriends} mutuals (${percentage(mutualFriends, addedFriends)}%)`)
+let friendsPageRoot = document.getElementsByClassName('js-react--friends-index')[0]
+const percentage = function(partial, total) {
+    return ((100 * partial) / total).toFixed(2)
+}
 
-        toolbarRow.style = 'align-items: center;'
+const setupMutualCounter = function() {
+    let body = document.body
+    let addedFriends = body.getElementsByClassName("user-card-brick").length
+    let mutualFriends = body.getElementsByClassName("user-card-brick--mutual").length
+    const toolbarRow = body.getElementsByClassName("user-list__toolbar-row")[0]
+    const toolbarItem = document.createElement('div')
+    const spacing = document.createElement('div')
+    const toolbarItemContent = document.createTextNode(`${mutualFriends}/${addedFriends} mutuals (${percentage(mutualFriends, addedFriends)}%)`)
 
-        spacing.classList.add('user-list__toolbar-item')
-        spacing.style = 'margin: auto;'
-        toolbarItem.appendChild(toolbarItemContent)
-        toolbarItem.classList.add('user-list__toolbar-item')
-        toolbarItem.style = 'padding: 0; font-size: 20px; font-weight: bold; margin: 5px;'
+    toolbarRow.style = 'align-items: center;'
 
-        const firstElementChild = toolbarRow.firstElementChild
-        toolbarRow.insertBefore(toolbarItem, firstElementChild)
-        toolbarRow.insertBefore(spacing, firstElementChild)
-    }
-})();
+    spacing.classList.add('user-list__toolbar-item')
+    spacing.style = 'margin: auto;'
+    toolbarItem.appendChild(toolbarItemContent)
+    toolbarItem.classList.add('user-list__toolbar-item')
+    toolbarItem.style = 'padding: 0; font-size: 20px; font-weight: bold; margin: 5px;'
+
+    const firstElementChild = toolbarRow.firstElementChild
+    toolbarRow.insertBefore(toolbarItem, firstElementChild)
+    toolbarRow.insertBefore(spacing, firstElementChild)
+}
+
+const observer = new MutationObserver(setupMutualCounter)
+observer.observe(friendsPageRoot, {
+    childList: true,
+    attributes: false,
+    subtree: false
+})
